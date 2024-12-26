03006ffd1fc7f9811d1e46f9b8b05018955ab374
esther hk rnd <esther.hk.rnd@gmail.com>
2024-12-26 13:08:06 +0200

    setup s3 storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,10 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource.js";
 import { data } from "./data/resource.js";
-import { firstBucket, secondBucket } from "./storage/resource";
+import { storage } from "./storage/resource";
 
 defineBackend({
   auth,
   data,
-  firstBucket,
-  secondBucket,
+  storage
 });
